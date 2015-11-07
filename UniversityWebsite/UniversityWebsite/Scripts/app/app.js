8aaf20bfae1688a87bae18cf44ca1407377187f7
Marcin <mfusiara@gmail.com>
2015-11-07 15:46:54 +0100

    szkielet panelu admina

---
+++ UniversityWebsite/UniversityWebsite/Scripts/app/app.js
@@ -0,0 +1,32 @@
+﻿var configApp = angular.module('configApp', ['ngRoute']);
+
+configApp.controller('LandingPageController', LandingPageController);
+
+configApp.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
+
+var configFunction = function ($routeProvider, $httpProvider) {
+    $routeProvider.
+        when('/dashboard', {
+            templateUrl: 'admin/dashboard'
+        }).
+        when('/users', {
+            templateUrl: 'admin/users'
+        }).
+        when('/pages', {
+            templateUrl: 'admin/pages'
+        }).
+        when('/subjects', {
+            templateUrl: 'admin/subjects'
+        }).
+        when('/navigationMenu', {
+            templateUrl: 'admin/navigationMenu'
+        }).
+        otherwise({
+            redirectTo: '/dashboard'
+        });
+
+    $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+}
+configFunction.$inject = ['$routeProvider', '$httpProvider'];
+
+configApp.config(configFunction);
