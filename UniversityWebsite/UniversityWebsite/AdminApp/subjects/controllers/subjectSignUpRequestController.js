4339a23b24f9ed03fe675a911358f8d2ea2f68a5
Marcin <mfusiara@gmail.com>
2015-12-27 01:02:09 +0100

    poprawione kafle

---
+++ UniversityWebsite/UniversityWebsite/AdminApp/subjects/controllers/subjectSignUpRequestController.js
@@ -0,0 +1,40 @@
+﻿angular.module('configApp.subjects')
+
+.controller('subjectSignUpRequestCtrl', function ($scope, subjectsService) {
+
+    loadSignUpRequsts();
+
+    function loadSignUpRequsts() {
+        subjectsService.getSignUpRequests({ subjectId: $scope.subject.Id })
+        .$promise.then(function (signUpRequests) {
+            $scope.signUpRequests = signUpRequests;
+        });
+    };
+
+    $scope.approve = function() {
+        var selectedRequests = getSelectedRequestIds($scope.signUpRequests);
+
+        subjectsService.approveSignUpRequests(selectedRequests).then(function(response) {
+            loadSignUpRequsts();
+        }, function(error) {
+            console.log("error");
+        });
+    };
+
+    $scope.reject = function() {
+        var selectedRequests = getSelectedRequestIds($scope.signUpRequests);
+        subjectsService.rejectSignUpRequests(selectedRequests).then(function (response) {
+            loadSignUpRequsts();
+        }, function (error) {
+            console.log("error");
+        });
+    };
+
+    function getSelectedRequestIds(signUpRequests) {
+        return Enumerable.From($scope.signUpRequests)
+            .Where(function (request) { return request.selected; })
+            .Select(function (request) { return request.Id; })
+            .ToArray();
+    };
+
+});
