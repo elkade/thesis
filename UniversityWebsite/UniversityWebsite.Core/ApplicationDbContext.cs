0808543a6cb93c2102abfee8bbcdfcc08c1ec0fb
Marcin <mfusiara@gmail.com>
2015-12-28 00:44:08 +0100

    poprawka po merge

--- UniversityWebsite/UniversityWebsite.Core/ApplicationDbContext.cs
+++ UniversityWebsite/UniversityWebsite.Core/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ namespace UniversityWebsite.Core
     public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext()
-            : base("DomainTestContext", throwIfV1Schema: false)
+            : base("DomainContext", throwIfV1Schema: false)
         {
         }
 
---
+++ UniversityWebsite/UniversityWebsite.Domain/Model/TeacherSubject.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniversityWebsite.Domain.Model
+{
+    public class TeacherSubject
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("TeacherId")]
+        public virtual User Teacher { get; set; }
+        public string TeacherId { get; set; }
+
+        [ForeignKey("SubjectId")]
+        public virtual Subject Subject { get; set; }
+        public int SubjectId { get; set; }
+
+    }
+}
