f61730db12b6a8da83126d057e0395145a7e7630
Marcin <mfusiara@gmail.com>
2016-01-03 13:07:30 +0100

    paginacja newsow

--- UniversityWebsite/UniversityWebsite/Model/SubjectVm.cs
+++ UniversityWebsite/UniversityWebsite/Model/SubjectVm.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UniversityWebsite.Api.Model;
 using UniversityWebsite.Model.Page;
 using UniversityWebsite.Services.Model;
 
@@ -7,10 +8,12 @@ namespace UniversityWebsite.Model
     public class SubjectVm
     {
         public string Name { get; set; }
+        public string UrlName { get; set; }
         public int SemesterNumber { get; set; }
         public List<string> Teachers { get; set; }
 
         public List<NewsVm> News { get; set; }
+        public PagedData<NewsVm> PaginateNews { get; set; } 
         public string Syllabus { get; set; }
         public string Schedule { get; set; }
         public List<FileDto> Files { get; set; }
